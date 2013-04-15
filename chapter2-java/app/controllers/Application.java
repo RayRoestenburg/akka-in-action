29eddf3a5aaaa07fb9343d32ec064a4b43f833cd
Muhammad <shipmaster@gmail.com>
2013-04-14 21:20:07 -0700

    added GSON serialization

--- chapter2-java/app/controllers/Application.java
+++ chapter2-java/app/controllers/Application.java
@@ -3,6 +3,10 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import models.Event;
+import java.util.List;
+import java.util.ArrayList;
+import com.google.gson.Gson;
 
 public class Application extends Controller {
   
@@ -15,7 +19,18 @@ public class Application extends Controller {
     }
   
     public static Result currentEvents() {
-    	return ok("current events: ");
+    	List events = new ArrayList<Event>();
+    	Event event = new Event();
+    	event.setName("test event");
+    	event.setNumberOfTickets(5);
+    	events.add(event);
+    	event = new Event();
+    	event.setName("test event 2");
+    	event.setNumberOfTickets(100);
+    	events.add(event);
+    	Gson gson = new Gson();
+    	String eventsJson=gson.toJson(events);
+    	return ok(eventsJson).as("application/json");
     }
   
 }
---
+++ chapter2-java/app/models/Event.java
@@ -0,0 +1,24 @@
+package models;
+
+public class Event {
+
+	private String name;
+	private int numberOfTickets;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setNumberOfTickets(int numberOfTickets) {
+		this.numberOfTickets = numberOfTickets;
+	}
+
+	public int getNumberOfTickets() {
+		return numberOfTickets;
+	}
+
+}
