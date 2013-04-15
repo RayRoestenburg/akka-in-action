07df3e9019570f42aa3104b7a284da8a75430c28
Rob Williams <rob@ontometrics.com>
2013-04-14 21:29:01 -0700

    Removing dupe methods from merge.

--- chapter2-java/app/controllers/Application.java
+++ chapter2-java/app/controllers/Application.java
@@ -15,33 +15,28 @@ import akka.actor.ActorRef;
 import akka.actor.Props;
 
 public class Application extends Controller {
-  
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
-    
-    public static Result newEvent() {
-    	return ok(index.render("created new event"));
-    }
-  
-    public static Result currentEvents() {
-    	List events = new ArrayList<Event>();
-    	Event event = new Event();
-    	event.setName("test event");
-    	event.setNumberOfTickets(5);
-    	events.add(event);
-    	event = new Event();
-    	event.setName("test event 2");
-    	event.setNumberOfTickets(100);
-    	events.add(event);
-    	Gson gson = new Gson();
-    	String eventsJson=gson.toJson(events);
-    	return ok(eventsJson).as("application/json");
-    }
-  
 
 	public static Result index() {
 		return ok(index.render("Your new application is ready."));
+	}
+
+	public static Result newEvent() {
+		return ok(index.render("created new event"));
+	}
+
+	public static Result currentEvents() {
+		List events = new ArrayList<Event>();
+		Event event = new Event();
+		event.setName("test event");
+		event.setNumberOfTickets(5);
+		events.add(event);
+		event = new Event();
+		event.setName("test event 2");
+		event.setNumberOfTickets(100);
+		events.add(event);
+		Gson gson = new Gson();
+		String eventsJson = gson.toJson(events);
+		return ok(eventsJson).as("application/json");
 	}
 
 	public static Result ping() {
@@ -54,14 +49,6 @@ public class Application extends Controller {
 						return ok(response.toString());
 					}
 				}));
-	}
-
-	public static Result newEvent() {
-		return ok(index.render("created new event"));
-	}
-
-	public static Result currentEvents() {
-		return ok("current events: ");
 	}
 
 }
