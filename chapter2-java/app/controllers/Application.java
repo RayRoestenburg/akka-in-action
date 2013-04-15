d794cc32a1dbf425370ee1bfc6851ba4614232d9
Rob Williams <rob@ontometrics.com>
2013-04-14 20:07:33 -0700

    Testing addition of other routes.

--- chapter2-java/app/controllers/Application.java
+++ chapter2-java/app/controllers/Application.java
@@ -1,14 +1,21 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
   
     public static Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+    
+    public static Result newEvent() {
+    	return ok(index.render("created new event"));
+    }
+  
+    public static Result currentEvents() {
+    	return ok("current events: ");
     }
   
 }
