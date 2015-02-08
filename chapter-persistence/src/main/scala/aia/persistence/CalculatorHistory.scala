22e48485eca5a77e786bf4f42fb6d67f19d1f7c7
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2015-02-08 13:54:24 +0100

    Added Calculator History example.

---
+++ chapter-persistence/src/main/scala/aia/persistence/CalculatorHistory.scala
@@ -0,0 +1,35 @@
+package aia.persistence
+
+import akka.actor._
+import akka.persistence._
+
+object CalculatorHistory {
+  def props = Props(new CalculatorHistory)
+  def name = "calculator-history"
+  case object GetHistory
+  case class History(added: Int = 0, subtracted: Int = 0, divided: Int = 0, multiplied: Int = 0) {
+    def incrementAdded = copy(added = added + 1)
+    def incrementSubtracted= copy(subtracted = subtracted + 1)
+    def incrementDivided = copy(divided = divided + 1)
+    def incrementMultiplied = copy(multiplied = multiplied + 1)
+  }
+}
+
+class CalculatorHistory extends PersistentView {
+  import Calculator._
+  import CalculatorHistory._
+
+  def viewId = CalculatorHistory.name
+
+  def persistenceId = Calculator.name
+
+  var history = History()
+
+  def receive = {
+    case event: Added => history = history.incrementAdded
+    case event: Subtracted => history = history.incrementSubtracted
+    case event: Divided => history = history.incrementDivided
+    case event: Multiplied => history = history.incrementMultiplied
+    case GetHistory => sender() ! history
+  }
+}
---
+++ chapter-persistence/src/test/scala/aia/persistence/CalculatorHistorySpec.scala
@@ -0,0 +1,55 @@
+package aia.persistence
+
+import akka.actor._
+import akka.persistence._
+
+import akka.testkit._
+import org.scalatest._
+
+class CalculatorHistorySpec extends PersistenceSpec(ActorSystem("test"))
+    with WordSpecLike
+    with PersistenceCleanup {
+
+  "The CalculatorHistory" should {
+    "Show the number of times every operation is used after starting" in {
+      val calc = system.actorOf(Calculator.props, Calculator.name)
+      calc ! Calculator.Add(1d)
+      calc ! Calculator.GetResult
+      expectMsg(1d)
+
+      calc ! Calculator.Subtract(0.5d)
+      calc ! Calculator.GetResult
+      expectMsg(0.5d)
+
+      calc ! Calculator.Multiply(2d)
+      calc ! Calculator.GetResult
+      expectMsg(1d)
+
+      calc ! Calculator.Multiply(4d)
+      calc ! Calculator.GetResult
+      expectMsg(4d)
+
+      calc ! Calculator.Divide(4d)
+      calc ! Calculator.GetResult
+      expectMsg(1d)
+
+      val calcHistory = system.actorOf(CalculatorHistory.props, CalculatorHistory.name)
+      calcHistory ! CalculatorHistory.GetHistory
+      expectMsg(CalculatorHistory.History(added = 1, subtracted = 1, multiplied = 2, divided = 1))
+      killActors(calc, calcHistory)
+    }
+
+    "Show the number of times every operation is used after it is forced to update" in {
+      val calc = system.actorOf(Calculator.props, Calculator.name)
+      val calcHistory = system.actorOf(CalculatorHistory.props, CalculatorHistory.name+"test")
+
+      calc ! Calculator.Add(1d)
+      calc ! Calculator.GetResult
+      expectMsg(2d)
+      calcHistory ! Update(await = true)
+      calcHistory ! CalculatorHistory.GetHistory
+      expectMsg(CalculatorHistory.History(added = 2, subtracted = 1, multiplied = 2, divided = 1))
+      killActors(calc, calcHistory)
+    }
+  }
+}
