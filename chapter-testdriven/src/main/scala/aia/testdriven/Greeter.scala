d1712682306a376df7c6ab03d94bc59818fa0d51
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2015-09-03 20:26:01 +0200

    Added code example to book.

--- chapter-testdriven/src/main/scala/aia/testdriven/Greeter.scala
+++ chapter-testdriven/src/main/scala/aia/testdriven/Greeter.scala
@@ -1,11 +1,13 @@
 package aia.testdriven
 
+//<start id="testdriven-greeter"/>
 import akka.actor.{ActorLogging, Actor}
 
 case class Greeting(message: String)
 
 class Greeter extends Actor with ActorLogging {
   def receive = {
-    case Greeting(message) => log.info("Hello {}!", message)
+    case Greeting(message) => log.info("Hello {}!", message) //<co id="say_hello_something"/>
   }
 }
+//<end id="testdriven-greeter"/>
