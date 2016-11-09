bddf583033a08eec734a7520230598b9ba592f57
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2016-11-09 21:28:15 +0200

    Commented test that always fails (in book as red-green-refactor example).

--- chapter-testdriven/src/test/scala/aia/testdriven/SilentActor01Test.scala
+++ chapter-testdriven/src/test/scala/aia/testdriven/SilentActor01Test.scala
@@ -10,13 +10,23 @@ class SilentActor01Test extends TestKit(ActorSystem("testsystem"))
   with WordSpecLike
   with MustMatchers
   with StopSystemAfterAll {
-
+  // Commented to make the travis build pass, this is the original test in the book
+  // "A Silent Actor" must {
+  //   "change state when it receives a message, single threaded" in {
+  //     //Write the test, first fail
+  //     fail("not implemented yet")
+  //   }
+  //   "change state when it receives a message, multi-threaded" in {
+  //     //Write the test, first fail
+  //     fail("not implemented yet")
+  //   }
+  // }
   "A Silent Actor" must {
-    "change state when it receives a message, single threaded" in {
+    "change state when it receives a message, single threaded" ignore {
       //Write the test, first fail
       fail("not implemented yet")
     }
-    "change state when it receives a message, multi-threaded" in {
+    "change state when it receives a message, multi-threaded" ignore {
       //Write the test, first fail
       fail("not implemented yet")
     }
